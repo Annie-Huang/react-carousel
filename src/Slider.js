830265ac410d240acafa756023c1238a22170136
Annie Huang <annie.hn@gmail.com>
2021-07-11 20:42:58 +1000

    Need to add all files in staging.

--- src/Slider.js
+++ src/Slider.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './slider.scss';
 
 const Slider = () => {
+  let sliderArr = [1, 2, 3, 4, 5];
+
   return (
     <div className='slider'>
-      <p>slider</p>
+      {sliderArr.map((item, index) => (
+        <div key={index} className='slide'>
+          {item}
+        </div>
+      ))}
     </div>
   );
 };
