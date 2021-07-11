34b8c0675eb021295f04c4237530f30f5ebb73c0
Annie Huang <annie.hn@gmail.com>
2021-07-11 21:00:40 +1000

    Add click left and right button logic and make sure x reflect the correct number.

--- src/Slider.js
+++ src/Slider.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './slider.scss';
 
 const Slider = () => {
   let sliderArr = [1, 2, 3, 4, 5];
 
+  const [x, setX] = useState(0);
+
+  const goLeft = () => {
+    console.log(x);
+    setX(x + 100);
+  };
+  const goRight = () => {
+    console.log(x);
+    setX(x - 100);
+  };
+
   return (
     <div className='slider'>
       {sliderArr.map((item, index) => (
-        <div key={index} className='slide'>
+        <div
+          key={index}
+          className='slide'
+          style={{ transform: `translateX(${x}%)` }}
+        >
           {item}
         </div>
       ))}
+      <button id='goLeft' onClick={goLeft}>
+        Left
+      </button>
+      <button id='goRight' onClick={goRight}>
+        Right
+      </button>
     </div>
   );
 };
